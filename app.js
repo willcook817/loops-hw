54db6605309de238dcfe3002bc298a0d8d28263e
willcook817 <OIC2@OIC2s-MacBook-Air.local>
2016-05-11 08:55:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//var x = "test"
+//alert(x)
+// array example
+//var dayOfWeek = ["mon","tues","wed","thurs","fri"]
+//document.write(dayOfWeek [2]);
+//console.log(dayOfWeek);
+// var gangs = ["BD","BDK","GD","GDK"]
+// document.write(gangs);
+
+// for(var i = 0; i<10; i++) {
+// 	console.log(i)
+// }
+
+// for(var w = 69; w<79; w++){
+// 	console.log(w)
+// }
+
+// x = 6
+// while(x < 10){
+// 	console.log("on number" + x)
+// 	x++;
+// }
+
+// var y = 1
+// while(y < 100 ){
+// 	console.log("I'm on number" + y)
+// 	y++;
+// }
+
+var x = 99
+while( x > 0 ){
+	console.log(x + " bottles of root beer on the wall ", + x + " bottles of root beer");
+	x--
+	console.log("...take one down pass it around " + x + " bottles of root beer");
+	console.log("");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
